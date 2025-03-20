ac680655bf29f882f061080b5217f46ccdf040a2
Erol <karisik.erol@qla.dev>
2025-03-21 00:23:55 +0100

    add_bottomtab_and_screens_for_navigation

--- frontend/screen/NearMeScreen.js
+++ frontend/screen/NearMeScreen.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import CustomMap from "../components/NearMe/Map"; 
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import CustomMap from "../components/NearMe/Map";
 
 export default function NearMeScreen() {
   return (
-    <View style={styles.container}>
-      <CustomMap />
-    </View>
+    
+      <View style={styles.mapContainer}>
+        <CustomMap />
+      </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  mapContainer: {
     flex: 1,
   },
 });
