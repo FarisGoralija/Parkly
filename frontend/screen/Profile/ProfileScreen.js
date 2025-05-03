93f4c94ea9019727ad6167ff072f459fed0136d3
Erol <karisik.erol@qla.dev>
2025-05-03 19:55:51 +0200

    add_log_out_modal

--- frontend/screen/Profile/ProfileScreen.js
+++ frontend/screen/Profile/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
 import GrayHeader from "../../components/common/GrayHeader";
 import ProfileOptionCard from "../../components/MyProfile/ProfileOptionCard";
 import BlueHeart from "../../components/svg/blueHeart";
@@ -9,16 +9,41 @@ import Card from "../../components/svg/Card";
 import LogOut from "../../components/svg/LogOut";
 
 export default function ProfileScreen() {
+  const handleLogoutPress = () => {
+    Alert.alert(
+      "Log out",
+      "Are you sure you would like to log out?",
+      [
+        {
+          text: "Cancel",
+          style: "destructive", // iOS red
+        },
+        {
+          text: "Confirm",
+          onPress: () => {
+            // Your logout logic here
+            console.log("Logged out");
+          },
+          style: "default", // iOS blue
+        },
+      ],
+      { cancelable: true }
+    );
+  };
+
   return (
     <View style={styles.container}>
       <GrayHeader title="Profile" />
       <View style={styles.content}>
         <ProfileOptionCard text="Profile details" icon={<User />} />
         <ProfileOptionCard text="Payment methods" icon={<Card />} />
-        <ProfileOptionCard text="Favourite parkings" icon={<BlueHeart/>} />
-        <ProfileOptionCard text="About us" icon={<AboutUs/>} />
-        <ProfileOptionCard text="Log out" icon={<LogOut />} />
-
+        <ProfileOptionCard text="Favourite parkings" icon={<BlueHeart />} />
+        <ProfileOptionCard text="About us" icon={<AboutUs />} />
+        <ProfileOptionCard
+          text="Log out"
+          icon={<LogOut />}
+          onPress={handleLogoutPress}
+        />
       </View>
     </View>
   );
