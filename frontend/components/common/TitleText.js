0fc8e4956fcd5926b1e5396ffb0e4a4b0c833d93
nedim71k <nedim.tahirovic3@gmail.com>
2025-05-06 12:08:18 +0200

    conflict_fix

--- frontend/components/common/TitleText.js
+++ frontend/components/common/TitleText.js
@@ -1,49 +1,48 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
-const TitleText = ({ title, subtitle, subtitleStyle, backIconStyle }) => {
+import BackButton from "../svg/backButton";
+ 
+const TitleText = ({
+  title,
+  subtitle,
+  subtitleStyle,
+  backIconStyle,
+  titleStyle,
+}) => {
   const navigation = useNavigation();
-
+ 
   return (
     <View style={styles.container}>
       <TouchableOpacity
         onPress={() => navigation.goBack()}
         style={styles.backButton}
       >
-        <Image
-          source={require("../../assets/icons/back.png")}
-          style={[styles.backIcon, backIconStyle]}
-        />
+        <BackButton style={backIconStyle} />
       </TouchableOpacity>
+ 
       <View style={styles.textContainer}>
-        <Text style={styles.title}>{title}</Text>
+        <Text style={[styles.title, titleStyle]}>{title}</Text>
         <Text style={[styles.subtitle, subtitleStyle]}>{subtitle}</Text>
       </View>
     </View>
   );
 };
-
+ 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     width: "100%",
     marginBottom: 20,
-    paddingTop: 20, // Ensure spacing consistency
+    paddingTop: 20,
   },
   backButton: {
     position: "absolute",
     left: 0,
-    top: 5, // Adjust for proper alignment
-  
-  },
-  backIcon: {
-    width: 63,
-    height: 42,
-    color: "white" // Adjusted for better scaling
+    top: 5,
   },
   textContainer: {
-    flex: 1, // Takes available space
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -52,17 +51,15 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     marginBottom: 20,
     textAlign: "center",
-    color: "white"
+    color: "black",
   },
   subtitle: {
     fontSize: 15,
-    color: "white",
+    color: "#9C9C9C",
     textAlign: "center",
     fontWeight: "400",
-    textAlign: 'center',
-    alignSelf: 'center',
-    
+    alignSelf: "center",
   },
 });
-
+ 
 export default TitleText;
--- frontend/navigation.js
+++ frontend/navigation.js
@@ -1,30 +1,49 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import BottomTabs from "./components/common/BottomTabs"; // Use BottomTabs instead of CustomBottomTab
+import BottomTabs from "./components/common/BottomTabs";
 import LoginScreen from "./screen/LoginScreen";
 import ForgotPasswordScreen from "./screen/ForgotPassword/ForgotPasswordScreen";
 import ForgotVerifyCodeScreen from "./screen/ForgotPassword/ForgotVerifyCodeScreen";
 import ForgotNewPasswordScreen from "./screen/ForgotPassword/ForgotNewPasswordScreen";
-import RegistrationUsernameScreen from "./screen/Registration/RegistrationUsernameScreen";
-import RegistrationPasswordScreen from "./screen/Registration/RegistrationPasswordScreen";
-
-
+import CarDetailsScreen from "./screen/MyCar/CarDetailsScreen";
+import { CarProvider } from "./context/CarContext";
+import { ParkingProvider } from "./context/ParkingContext";
+import ProfileScreen from "./screen/Profile/ProfileScreen";
+import FavouriteScreen from "./screen/Profile/FavouriteScreen";
+import AboutUsScreen from "./screen/Profile/AboutUsScreen";
+import ProfileDetailsScreen from "./screen/Profile/ProfileDetailsScreen";
+import MyCardScreen from "./screen/Profile/MyCardScreen";
+import CardDetails from "./screen/Profile/CardDetailsScreen";
+import { CardProvider } from "./context/CardContext";
+ 
+ 
 const Stack = createStackNavigator();
-
+ 
 export default function Navigation() {
   return (
     <NavigationContainer>
+       <ParkingProvider>
+       <CardProvider>
+       <CarProvider>
       <Stack.Navigator screenOptions={{ headerShown: false }}
-      initialRouteName="LoginScreen">
-        <Stack.Screen name="Home" component={BottomTabs} /> 
+      initialRouteName="Home">
+        <Stack.Screen name="Home" component={BottomTabs} />
         <Stack.Screen name="LoginScreen" component={LoginScreen}/>
-        <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} /> 
-        <Stack.Screen name="ForgotVerifyCodeScreen" component={ForgotVerifyCodeScreen} /> 
-        <Stack.Screen name="ForgotNewPasswordScreen" component={ForgotNewPasswordScreen} /> 
-        <Stack.Screen name="RegistrationUsernameScreen" component={RegistrationUsernameScreen} /> 
-        <Stack.Screen name="RegistrationPasswordScreen" component={RegistrationPasswordScreen} /> 
+        <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} />
+        <Stack.Screen name="ForgotVerifyCodeScreen" component={ForgotVerifyCodeScreen} />
+        <Stack.Screen name="ForgotNewPasswordScreen" component={ForgotNewPasswordScreen} />
+        <Stack.Screen name="CarDetailsScreen" component={CarDetailsScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="FavouriteScreen" component={FavouriteScreen} />
+        <Stack.Screen name="AboutUsScreen" component={AboutUsScreen} />
+        <Stack.Screen name="ProfileDetailsScreen" component={ProfileDetailsScreen} />
+        <Stack.Screen name="MyCardScreen" component={MyCardScreen} />
+        <Stack.Screen name="CardDetails" component={CardDetails} />
       </Stack.Navigator>
+      </CarProvider>
+      </CardProvider>
+      </ParkingProvider>
     </NavigationContainer>
   );
 }
