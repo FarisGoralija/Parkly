8aa7ede6bcd1f5b66fdd911d7ae03bb60cbc8c71
Faris <farisg55@gmail.com>
2025-05-06 15:45:58 +0200

    Fixed the namespace of the Auth controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\Api\ParkingController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('parkings', ParkingController::class);
 Route::post('register', [AuthController::class, 'register']);
-//Route::get('users', [AuthController::class, 'getUsers']);
-Route::middleware('auth:sanctum')->get('users', [AuthController::class, 'getUsers']);
+Route::get('users', [AuthController::class, 'getUsers']);
+//Route::middleware('auth:sanctum')->get('users', [AuthController::class, 'getUsers']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
