82283d7f176c29fa22b50db9ca0ca134f010d0e9
keraaa92 <121890183+keraaa92@users.noreply.github.com>
2025-05-13 14:51:15 +0200

    Adding routes

---
+++ backend/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+
+use App\Http\Controllers\Api\ParkingController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\ParkingSpotController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::get('users', [AuthController::class, 'getUsers']);
+//Route::middleware('auth:sanctum')->get('users', [AuthController::class, 'getUsers']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+
+
+//Parking routes
+Route::apiResource('parkings', ParkingController::class)
+    ->only(['index', 'show', 'store', 'destroy']);
+
+//Parking spots routes
+Route::apiResource('parkings.parking-spots', ParkingSpotController::class)
+    ->scoped();
+
+//Car routes
+Route::apiResource('users.cars', CarController::class)
+    ->scoped()
+    ->only(['index', 'store', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
