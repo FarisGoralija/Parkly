cc2c4c27732fb3325b245e7eded47749fdbbe67d
Erol <karisik.erol@qla.dev>
2025-05-14 00:53:33 +0200

    conflict_merge

--- frontend/api/endpoints.js
+++ frontend/api/endpoints.js
@@ -1,14 +1,17 @@
-const BASE_URL = "https://parkly-production.up.railway.app/api";
-
 const endpoints = {
   parking: `${BASE_URL}/parkings`,
   getParkingById: (id) => `${BASE_URL}/parkings/${id}`, 
   registerUser: `${BASE_URL}/register`,
   login: `${BASE_URL}/login`,
-  getUserCars: (userId) => `${BASE_URL}/users/${userId}/cars`,            // GET all
-  addUserCar: (userId) => `${BASE_URL}/users/${userId}/cars`,             // POST
-  getSingleCar: (userId, carId) => `${BASE_URL}/users/${userId}/cars/${carId}`, // GET one
-  deleteUserCar: (userId, carId) => `${BASE_URL}/users/${userId}/cars/${carId}` // DELETE
-};
 
-export default endpoints;
+  // Car API
+  getUserCars: (userId) => `${BASE_URL}/users/${userId}/cars`,
+  addUserCar: (userId) => `${BASE_URL}/users/${userId}/cars`,
+  getSingleCar: (userId, carId) => `${BASE_URL}/users/${userId}/cars/${carId}`,
+  deleteUserCar: (userId, carId) => `${BASE_URL}/users/${userId}/cars/${carId}`,
+
+  // Password reset
+  forgotPassword: `${BASE_URL}/forgot-password`,
+  resetPassword: `${BASE_URL}/reset-password`,
+  verifyCode: `${BASE_URL}/verify-reset-code`
+};
