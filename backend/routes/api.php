09cdafbb11a6ae5f833b707836911311394ac353
keraaa92 <121890183+keraaa92@users.noreply.github.com>
2025-05-21 11:31:48 +0200

    added_new_type_hint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,7 +12,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UpdateProfileController;
 use Laravel\Socialite\Facades\Socialite;
-use Laravel\Socialite\Facades\AbstractProvider;
+use Laravel\Socialite\Two\AbstractProvider;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
